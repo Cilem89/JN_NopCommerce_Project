7dd2ba3bc6b1eaf31b67df0a3f535b8f68a9a04e
12Esmaa <esmadengesikm@icloud.com>
2024-04-05 02:13:44 +0300

    düzen

--- src/US_503_PozitifNegativeLogin/US_503_PozitifNegativeLoginPOM.java
+++ src/US_503_PozitifNegativeLogin/US_503_PozitifNegativeLoginPOM.java
@@ -1,35 +1,30 @@
 package US_503_PozitifNegativeLogin;
 
+
+
 import Utility.BaseDriver;
-import Utility.BaseDriverParameter;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 public class US_503_PozitifNegativeLoginPOM extends BaseDriver {
-    @Test(dataProvider="datalarim")
+    @Test(dataProvider = "datalarim")
     public void US_503(String username, String password) {
         US_503_PozitifNegativeLoginElements pnl = new US_503_PozitifNegativeLoginElements();
         driver.get("https://demo.nopcommerce.com/");
 
         SoftAssert softAssert = new SoftAssert();
 
-        pnl.loginButton.click();
+        pnl.login.click();
         softAssert.assertNotNull(pnl.loginButton2, "Login page couldn't displayed!");
         wait.until(ExpectedConditions.visibilityOf(pnl.email));
         pnl.email.sendKeys(username);
         pnl.password.sendKeys(password);
         pnl.loginButton2.click();
-        if (pnl.email.isEnabled()) {
-            softAssert.assertTrue(pnl.emailError.isDisplayed(), "Email warning couldn't displayed!");
-        } else if (pnl.email.equals("javanator@gmail.com") && password.equals("javanator123")) {
-            softAssert.assertTrue(pnl.logoutButton.isDisplayed(), "Login failed!");
-        } else {
-            softAssert.assertTrue(pnl.failedLogin.isDisplayed(), "Failed login warning couldn't displayed!");
-        }
-       // softAssert.assertAll();
-    }
+
+
+}
 
     @DataProvider
     Object[][] datalarim(){
