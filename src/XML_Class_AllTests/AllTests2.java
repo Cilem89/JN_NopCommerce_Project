1c7078af9c67d041d1787801353129e746507565
cilem89 <okkalicilem@gmail.com>
2024-04-05 03:29:06 +0300

    Tüm Adımlar tamamlandı

--- src/XML_Class_AllTests/AllTests2.java
+++ src/XML_Class_AllTests/AllTests2.java
@@ -1,6 +1,7 @@
 package XML_Class_AllTests;
 
 import US_501_Register.US_501_Register_Element;
+import US_503_PozitifNegativeLogin.US_503_PozitifNegativeLoginElements;
 import US_504_Tab_Menu_Kontroll.US_504_Tab_Menu_Elements;
 import US_505_TabMenuUrunKontrol.US_505_TabMenuUrunKontrol_Elements;
 import US_506_Tab.US_506_TabElements;
@@ -18,6 +19,7 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
@@ -28,7 +30,7 @@ import java.util.List;
 
 import static Utility.Tools.ListContainsString;
 
-public class AllTests extends BaseDriver {
+public class AllTests2 extends BaseDriver {
     @Test(groups = {"Registration","Smoke"},priority = 1)
     public void  _501_POM(){
         US_501_Register_Element re=new US_501_Register_Element();
@@ -54,21 +56,49 @@ public class AllTests extends BaseDriver {
         re.confirmPassword.sendKeys("javanator123");
         re.registerButton.click();
     }
-    @Test(dependsOnMethods = {"_501_POM"},groups = {"LoginTest","Smoke"},priority = 2)
-            public void _502_POM(){
+    @Test(dataProvider = "datalarim",groups = {"LoginTest","Smoke"},priority = 2)
+    public void _503_POM(String username, String password){
+
+        US_503_PozitifNegativeLoginElements pnl = new US_503_PozitifNegativeLoginElements();
+        driver.get("https://demo.nopcommerce.com/");
+
+        SoftAssert softAssert = new SoftAssert();
+
+        pnl.login.click();
+        softAssert.assertNotNull(pnl.loginButton2, "Login page couldn't displayed!");
+        wait.until(ExpectedConditions.visibilityOf(pnl.email));
+        pnl.email.sendKeys(username);
+        pnl.password.sendKeys(password);
+        pnl.loginButton2.click();
+    }
+    @DataProvider
+    Object[][] datalarim(){
+        Object[][] usernameAndPassword =
+                {
+                        {"User", "12345"},
+                        {"Esma", "4444"},
+                        {"Mutalip", "6666"},
+                        {"Salih", "999"},
+                        {"Akman", "ddddd"},
+                        {"Ayşe", "777"},
+                        {"javanator@gmail.com", "javanator123"}
+
+                };
+        return usernameAndPassword;
+    }
+
+    @Test(groups = {"LoginTest","Smoke"},priority = 3)
+    public void _502_POM(){
+        US_503_PozitifNegativeLoginElements pnl=new US_503_PozitifNegativeLoginElements();
+        pnl.logoutButton.click();
         LoginMethod.LoginTest();
     }
 
-    @Test(dependsOnMethods = {"_501_POM"},groups = {"LoginTest","Smoke"},priority = 3)
-    public void _503_POM(){
-
-    }
-
-    @Test(dependsOnMethods = {"_501_POM","_502_POM"},groups = {"UITesting","TAB Menu"},priority = 4)
+    @Test(groups = {"UITesting","TAB Menu"},priority = 4)
     @Parameters("Text")
     public void _504_POM(String searchTxt) {
         US_504_Tab_Menu_Elements tm = new US_504_Tab_Menu_Elements();
-       LoginMethod.LoginTest();
+
         driver.get("https://demo.nopcommerce.com/");
         Tools.Bekle(2);
 
@@ -105,12 +135,12 @@ public class AllTests extends BaseDriver {
         Assert.assertTrue(butunUrunler.contains(tm.productName.getText()));
     }
 
-    @Test(dependsOnMethods = {"_501_POM","_502_POM"},groups = {"UITesting","Search", "TAB Menu","Regression"},priority = 5)
+    @Test(groups = {"UITesting","Search", "TAB Menu","Regression"},priority = 5)
     @Parameters("Searchtext")
     public void _505_POM(String search) {
         US_505_TabMenuUrunKontrol_Elements tmuk = new US_505_TabMenuUrunKontrol_Elements();
         Actions actions = new Actions(BaseDriver.driver);
-        LoginMethod.LoginTest();
+
 
         List<String> tabMenuTexts = new ArrayList<>();
         for (WebElement tabMenuItem : tmuk.tabMenuItems) {
@@ -136,64 +166,64 @@ public class AllTests extends BaseDriver {
         Assert.assertTrue(tmuk.searchResults.contains(tmuk.searchResults.get(randomSecim)),"seçilen ürün bulunamadı");
     }
 
-    @Test(dependsOnMethods = {"_501_POM","_502_POM"},groups = {"UITesting", "TAB Menu", "Order"}, priority = 6)
+    @Test(groups = {"UITesting", "TAB Menu", "Order"}, priority = 6)
     public void TC_506_POM() {
-        driver.get("https://demo.nopcommerce.com/");
-        US_506_TabElements tab= new US_506_TabElements();
-        LoginMethod.LoginTest();
+            driver.get("https://demo.nopcommerce.com/");
+            US_506_TabElements tab= new US_506_TabElements();
 
-        SoftAssert softAssert = new SoftAssert();
+            SoftAssert softAssert = new SoftAssert();
 
-        tab.giftCardsButton.click();
-        int random = Tools.randomGenerator(tab.giftCards.size() );
-        String cardName = tab.giftCards.get(random).getText();
-        tab.giftCards.get(random);
-        Tools.Bekle(1);
-
-
-        tab.addToCart.click();
-        wait.until(ExpectedConditions.visibilityOf(tab.recipientName));
-        JavascriptExecutor js=(JavascriptExecutor) driver;
-
-        js.executeScript("window.scrollTo(0,500)");
+            tab.giftCardsButton.click();
+            int random = Tools.randomGenerator(tab.giftCards.size() );
+            String cardName = tab.giftCards.get(random).getText();
+            tab.giftCards.get(random);
+            Tools.Bekle(1);
 
 
-        tab.recipientName.sendKeys("esma");
-        tab.recipientEmail.sendKeys("esma@gmail.com");
-        tab.yourName.sendKeys("esma dengeşik");
-        tab.yourEmail.sendKeys("dengeşik@gmail.com");
-        tab.addToCart.click();
-        wait.until(ExpectedConditions.visibilityOf(tab.giftSuccess));
-        softAssert.assertTrue(tab.giftSuccess.getText().contains("The product has been added to your"), "The addition to the cart process failed!");
-        Tools.JSClick(tab.cart);
-        softAssert.assertTrue(ListContainsString(tab.cartItems, cardName), "The added gift card could not be displayed in the cart!");
+            tab.addToCart.click();
+            wait.until(ExpectedConditions.visibilityOf(tab.recipientName));
+            JavascriptExecutor js=(JavascriptExecutor) driver;
 
-    }
+            js.executeScript("window.scrollTo(0,500)");
 
-    @Test(dependsOnMethods = {"_501_POM","_502_POM"},groups = {"UITesting","Tab Menu","Order"},priority = 7)
+
+            tab.recipientName.sendKeys("esma");
+            tab.recipientEmail.sendKeys("esma@gmail.com");
+            tab.yourName.clear();
+            tab.yourEmail.clear();
+            tab.yourName.sendKeys("esma dengeşik");
+            tab.yourEmail.sendKeys("dengeşik@gmail.com");
+            tab.addToCart.click();
+            wait.until(ExpectedConditions.visibilityOf(tab.giftSuccess));
+            softAssert.assertTrue(tab.giftSuccess.getText().contains("The product has been added to your"), "The addition to the cart process failed!");
+            Tools.JSClick(tab.cart);
+            softAssert.assertTrue(ListContainsString(tab.cartItems, cardName), "The added gift card could not be displayed in the cart!");
+        }
+
+    @Test(groups = {"UITesting","Tab Menu","Order"},priority = 7)
     public void _507_POM(){
-      LoginMethod.LoginTest();
+
         US_507_Siparis_Elements e=new US_507_Siparis_Elements();
 
         driver.get("https://demo.nopcommerce.com/");
 
         e.computers.click();
         e.desktops.click();
-        e.owncomputer.click();
-        e.ram.click();
-        e.ram8gb.click();
-        e.rom.click();
-        e.cartbtn.click();
+        Tools.JSClick(e.owncomputer);
+        Tools.JSClick(e.ram);
+        Tools.JSClick(e.ram8gb);
+        Tools.JSClick(e.rom);
+        Tools.JSClick(e.cartbtn);
 
         Assert.assertTrue(e.verify.getText().toLowerCase().contains("the product has been added to your"),"basarisiz");
 
     }
 
-    @Test(dependsOnMethods = {"_501_POM","_502_POM"},groups = {"UITesting","Search","Smoke","Regression"},priority = 8)
+    @Test(groups = {"UITesting","Search","Smoke","Regression"},priority = 8)
     @Parameters("searchtext")
     public void _508_POM(String arananKelime){
         US_508_Parametreli_Elements e=new US_508_Parametreli_Elements();
-        LoginMethod.LoginTest();
+
         driver.get("https://demo.nopcommerce.com/");
 
         e.Search.sendKeys(arananKelime, Keys.ENTER);
